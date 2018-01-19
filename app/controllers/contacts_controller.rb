d7ce933685504254e54a0df03d8cf3a7e76116e8
Kevin Lawrence <lawrence.kvn@gmail.com>
2018-01-19 19:02:05 +0000

    Added contact create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,26 +1,18 @@
 class ContactsController < ApplicationController
     def new
-        @contact = Contact.new
-        #Now everytime they go to the form and add an object (def new) it'll save
-        #it to the instance vailable (@contact) 
-    end
-    
-    def create 
-        @contact = Contact.new(contact_params)
-        if @contact.save
-            redirect_to new_contact_path, notice: "Message sent."
-        else
-            redirect_to new_contact_path, notice: "Error occured."
-        end
-            #if the save is successful, redirect the user back to a 
-            #new contact path(blank form) w/ a message. Else back to form w/ error
-            #added place to display messages in view file
-    end
-    #^code to actually get it to save: def create ... end
-    
-    private
-        def contact_params
-           params.require(:contact).permit(:name, :email, :comments) 
-        end
+  @contact = Contact.new
+end
+def create
+  @contact = Contact.new(contact_params)
+  if @contact.save
+     redirect_to new_contact_path, notice: "Message sent."
+  else
+     redirect_to new_contact_path, notice: "Error occured."
+  end
+end
+private
+  def contact_params
+     params.require(:contact).permit(:name, :email, :comments)
+  end
         #Adds requirement
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,2 @@
-class CreateContacts < ActiveRecord::Migration[5.0]
-  def change
-    create_table :contacts do |t|
-        t.string "name"
-        t.string "email"
-        t.text "comments
-        t.datetime "created_at", null:false
-        t.datetime "updated_at", null: false
-    end
-  end
+class Contact < ActiveRecord::Base
 end
