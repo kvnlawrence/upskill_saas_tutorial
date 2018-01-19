21362574273f457291567ca952fbae0d18c91fbf
Kevin Lawrence <lawrence.kvn@gmail.com>
2018-01-19 18:50:41 +0000

    Trying to solve no route match error

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,11 @@
-class Contact < ActiveRecord::Base
-    
+class CreateContacts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :contacts do |t|
+        t.string "name"
+        t.string "email"
+        t.text "comments
+        t.datetime "created_at", null:false
+        t.datetime "updated_at", null: false
+    end
+  end
 end
