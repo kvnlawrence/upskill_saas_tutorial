99fd7e7e2909c9d104e17c38887f87e577f2f9ae
Kevin Lawrence <lawrence.kvn@gmail.com>
2018-01-19 17:28:44 +0000

    Created contacts table

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
  root to: 'pages#home'
  get 'about', to: 'pages#about'
  resources :contacts
+  # ^This auto generated many lines when you check 'rails routes' in terminal.
+  # including /new_contact, edit_contact, and many more. 
  end
+
