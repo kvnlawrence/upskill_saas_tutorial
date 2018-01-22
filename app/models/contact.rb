9a9c7389038afc50c6b83763626712e6d8f7136f
Kevin Lawrence <lawrence.kvn@gmail.com>
2018-01-22 23:16:31 +0000

    Added form validations to model. Deleted a contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,12 @@
+=begin 
+ Blueprint file for any contact object
+ Objects = the list; name, email, etc. It knows this list based on how we 
+ configured the database.
+=end
+
 class Contact < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :comments, presence: true
 end
+
