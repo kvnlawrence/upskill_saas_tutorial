63cc627a178b7acb421d5dc137228c6720602db6
Kevin Lawrence <lawrence.kvn@gmail.com>
2018-01-22 23:25:26 +0000

    Improved error messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,12 +2,19 @@ class ContactsController < ApplicationController
     def new
   @contact = Contact.new
 end
+
 def create
   @contact = Contact.new(contact_params)
+  # This is mass assigning each contact w/ fields w/ the params.require below
   if @contact.save
-     redirect_to new_contact_path, notice: "Message sent."
+    flash[:success] = "Message sent."
+     redirect_to new_contact_path
   else
-     redirect_to new_contact_path, notice: "Error occured."
+    flash[:error] = @contact.errors.full_messages.join(", ")
+    #.join takes anything from an array--[x..y]--into a string
+    #full_messages.join is providing all rails expected messages 'name can't be
+    #blank, emails can't be blank, etc.
+     redirect_to new_contact_path
   end
 end
 private
