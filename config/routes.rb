260a655e16e63fda92284e034fa409be7cb30891
Kevin Lawrence <lawrence.kvn@gmail.com>
2018-01-24 19:47:30 +0000

    added new url for contact us page and fixed a couple bugs

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Rails.application.routes.draw do
  resources :contacts
   # ^This auto generated many lines when you check 'rails routes' in terminal.
   # including /new_contact, edit_contact, and many more. 
- get 'contact-us' to: 'contacts#new'
- # ^Changes URL from contacts/new to contact-us
  end
 
