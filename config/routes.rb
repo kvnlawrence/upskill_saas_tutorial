45ef0f68054a260926b9622cb584ed4630ebd993
Kevin Lawrence <lawrence.kvn@gmail.com>
2018-01-24 20:06:30 +0000

    Double checked routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
+# Main file for listing out different urls/paths a customer can go to
+
 Rails.application.routes.draw do
  root to: 'pages#home'
  get 'about', to: 'pages#about'
  resources :contacts
   # ^This auto generated many lines when you check 'rails routes' in terminal.
   # including /new_contact, edit_contact, and many more. 
+ get 'contact-us', to: 'contacts#new'
+   # ^Changes URL from contacts/new to contact-us
  end
 
