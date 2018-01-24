5cdf99923de155eb22edae372a1dc757bf80c21e
Kevin Lawrence <lawrence.kvn@gmail.com>
2018-01-24 19:32:19 +0000

    not sure

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
  resources :contacts
   # ^This auto generated many lines when you check 'rails routes' in terminal.
   # including /new_contact, edit_contact, and many more. 
+ get 'contact-us' to: 'contacts#new'
+ # ^Changes URL from contacts/new to contact-us
  end
 
