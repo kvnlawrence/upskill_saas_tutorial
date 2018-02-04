ff8f8ece3edce2f5d708444e2a988712fb4980ec
Kevin Lawrence <lawrence.kvn@gmail.com>
2018-02-04 17:58:06 +0000

    Added contact form mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,4 +1,4 @@
-Class Contact Mailer < ActionMailer::Base
+Class ContactMailer < ActionMailer::Base
     default to: 'lawrence.kvn@gmail.com'
     
     def contact.email(name, email, body)
@@ -9,3 +9,27 @@ Class Contact Mailer < ActionMailer::Base
         mail(from: email, subject: 'Contact Form Message')
     end
 end
+
+=begin
+NOTES ON 'CLASS' (example Class ContactMailer:
+Class is a chunck of code related to one thing.
+Class is "just catorgizing your code into logical groups. You're classifying your code"
+Here it's related to the functionality of sending emails.
+
+The arrow opporator '<' say's it's inhareting another class(here ActionMailer written by rails)
+    You could go read actionmailer in the rails code
+
+Function: Functions start w/ def. 
+    Functions aren't a set of rules but instructions
+    Functions inside classes are called 'methods' or 'actions'
+    
+This function has each entry in the form associated w/ a variable (name, email, etc.)
+Placing something next to a function name like contact_email(name,email) is called paramaters(also called arguements)
+once you added them you can use them below. 
+
+We'll use all three instance variables in email html
+template view. 
+
+Customer enters in form fields, those values pop into the function and are assigned to 
+    corrosponding variables. 
+=end
