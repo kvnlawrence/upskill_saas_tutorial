7bd3a0a7b38ce99d33c2b0a97f323eccb09fe980
Kevin Lawrence <lawrence.kvn@gmail.com>
2018-02-23 16:49:51 +0000

    Added contact form mailer file hopefully fixing heroku error

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,13 +1,11 @@
-Class ContactMailer < ActionMailer::Base
-    default to: 'lawrence.kvn@gmail.com'
-    
-    def contact.email(name, email, body)
-        @name = name
-        @email = email
-        @body = body
-        
-        mail(from: email, subject: 'Contact Form Message')
-    end
+class ContactMailer < ActionMailer::Base
+  default to: 'lawrence.kvn@gmail.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end
 
 =begin
