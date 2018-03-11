c460ac2a96496002da861ab7d9639d82c10a3a0d
Kevin Lawrence <lawrence.kvn@gmail.com>
2018-03-11 05:06:00 +0000

    fixed routes file

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,16 @@ Points to file in folders to the left. pages#about -> pages/about.html.erb
 =end
 
 Rails.application.routes.draw do
-  devise_for :users
- root to: 'pages#home'
- get 'about', to: 'pages#about'
- resources :contacts, only: :create
-  # ^This auto generated many lines when you check 'rails routes' in terminal.
-  # including /new_contact, edit_contact, and many more.  
- get 'contact-us', to: 'contacts#new', as: 'new_contact'
-   # ^Changes URL from contacts/new to contact-us
- get 'my-hubspot', to: 'pages#myhubspot'
- end
+  root to: 'pages#home'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users do
+    resource :profile
+  end
+  get 'about', to: 'pages#about'
+  resources :contacts, only: :create
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  get 'myhubspot', to: 'pages#myhubspot'
+end
 
 # Get requests are the most common requests, sent anytime you go to x.com 
  
