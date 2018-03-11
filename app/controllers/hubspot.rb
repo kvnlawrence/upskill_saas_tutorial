663b65b5af7a575345aa04c88c553d8909d42824
Kevin Lawrence <lawrence.kvn@gmail.com>
2018-03-11 04:42:34 +0000

    deleted old ruby.rb file

--- app/controllers/hubspot.rb
+++
@@ -1,7 +0,0 @@
-class HubspotController < ApplicationController
-    
-    def show
-         @contacts = (Hubspot::Contact.all).each do |contact|
-                info = contact.properties
-    end
-end 
